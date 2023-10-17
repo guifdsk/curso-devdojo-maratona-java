4249f5ecfb8b48d07c48f8d4841edad4d8565906
Guilherme Fidelis Pereira <guilhermefdsk.msl@outlook.com>
2023-10-17 07:45:14 -0300

    feat: tipos primitivos - convencoes de variaveis - #AULA001

---
+++ curso-devdojo-maratona-java/src/main/java/io/github/guifdsk/curso/application/aula001TiposPrimitivos/Aula001TiposPrimitivos.java
@@ -0,0 +1,12 @@
+package io.github.guifdsk.curso.application.aula001TiposPrimitivos;
+
+public class Aula001TiposPrimitivos {
+    public static void main(String[] args) {
+/*
+        Tipo primitivos:
+        int, double, float, char, byte, short, long, boolean
+*/
+        int idade = 10;
+        System.out.println("A idade é: " + idade + " anos.");
+    }
+}
