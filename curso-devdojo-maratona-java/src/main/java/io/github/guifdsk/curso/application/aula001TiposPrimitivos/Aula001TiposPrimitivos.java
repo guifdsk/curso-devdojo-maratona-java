c770da340ce5f2ccedbde7f335cd52540d30a159
Guilherme Fidelis Pereira <guilhermefdsk.msl@outlook.com>
2023-10-17 08:13:00 -0300

    feat: tipos primitivos - declaracao e tamanho em memoria - #AULA001

--- curso-devdojo-maratona-java/src/main/java/io/github/guifdsk/curso/application/aula001TiposPrimitivos/Aula001TiposPrimitivos.java
+++ curso-devdojo-maratona-java/src/main/java/io/github/guifdsk/curso/application/aula001TiposPrimitivos/Aula001TiposPrimitivos.java
@@ -7,6 +7,23 @@ public class Aula001TiposPrimitivos {
         int, double, float, char, byte, short, long, boolean
 */
         int idade = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 65;
+
         System.out.println("A idade é: " + idade + " anos.");
+        System.out.println(numeroGrande);
+        System.out.println(salarioDouble);
+        System.out.println(salarioFloat);
+        System.out.println(idadeByte);
+        System.out.println(idadeShort);
+        System.out.println(verdadeiro);
+        System.out.println(falso);
+        System.out.println(caractere);
     }
 }
