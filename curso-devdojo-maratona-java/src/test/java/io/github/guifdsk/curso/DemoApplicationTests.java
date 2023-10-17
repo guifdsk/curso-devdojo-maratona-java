a528d30aa928d0b2b749aa4436b1cea093e22cba
Guilherme Fidelis Pereira <guilhermefdsk.msl@outlook.com>
2023-10-17 01:34:59 -0300

    feat: adicionando configuracoes iniciais do repositorio

---
+++ curso-devdojo-maratona-java/src/test/java/io/github/guifdsk/curso/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.guifdsk.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
