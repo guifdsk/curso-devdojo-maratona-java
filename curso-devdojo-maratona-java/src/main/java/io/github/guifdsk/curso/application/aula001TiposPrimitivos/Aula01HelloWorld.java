488b9867c7cc80d96e2546caaaa2aee22625584b
Guilherme Fidelis Pereira <guilhermefdsk.msl@outlook.com>
2023-10-30 21:17:12 -0300

    fix: ajuste organização aulas

---
+++ curso-devdojo-maratona-java/src/main/java/io/github/guifdsk/curso/application/aula001TiposPrimitivos/Aula01HelloWorld.java
@@ -0,0 +1,7 @@
+package io.github.guifdsk.curso.application.aula001TiposPrimitivos;
+
+public class Aula01HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
